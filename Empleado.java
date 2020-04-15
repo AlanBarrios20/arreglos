784f10b22b74198ee88d385f774046c59f5e47dd
Alan Barrios <alanbarraza973@gmail.com>
2020-04-14 19:28:00 -0500

    Se crea la Clase Empleado

---
+++ Empleado.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Empleado here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+// La palabra reservada extens hace referencia a la Herencia en Programacion
+// orientada a Objeto
+public class Empleado extends Persona
+{
+    // instance variables - replace the example below with your own
+   
+
+    /**
+     * Constructor for objects of class Empleado
+     */
+    public Empleado()
+    {
+      
+    }
+   
+}
+   
